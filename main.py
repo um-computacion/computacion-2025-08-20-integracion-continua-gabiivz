afaccc2b42b6c3eb88a0168bf181c431f169f306
gabiivz <g.vaca@alumno.um.edu.ar>
2025-08-27 08:42:40 -0300

    Resta y test resta

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def suma (a, b) :
+
+    return a + b
+
+    print(suma(2, 3))
+
+def resta (a, b) :
+
+    return a - b
--- suma.py
+++
@@ -1,6 +0,0 @@
-
-def suma (a, b) :
-
-    return a + b
-if __name__== "__main__":
-    print(suma(2, 3))
